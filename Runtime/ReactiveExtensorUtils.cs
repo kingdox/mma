90529baf1a165a042df4d9c58470fb745cc66d82
Xavier Arpa <arpaxavier@gmail.com>
2022-06-11 21:15:43 +0200

    Se hace las validaciones

--- Runtime/ReactiveExtensorUtils.cs
+++ Runtime/ReactiveExtensorUtils.cs
@@ -1,33 +1,30 @@
 using System;
 using UnityEngine.Events;
-namespace X
+public static partial class ReactiveExtensorUtils 
 {
-    public static partial class ReactiveExtensorUtils 
+    /// <summary>
+    /// Apply a subscription to a specified Action, that can be a method group
+    /// </summary>
+    public static void Subscribe<T>(this bool condition, ref UnityAction<T> del, UnityAction<T> action)
     {
-        /// <summary>
-        /// Apply a subscription to a specified Action, that can be a method group
-        /// </summary>
-        public static void Subscribe<T>(this bool condition, ref UnityAction<T> del, UnityAction<T> action)
-        {
-            if (condition) del += action;
-            else del -= action;
-        }
-
-        public static void Subscribe<T>(this bool condition, ref Action<T> del, Action<T> action)
-        {
-            if (condition) del += action;
-            else del -= action;
-        }
-
-        /// <summary>
-        /// Apply a subscription to a specified Action, that can be a method group
-        /// </summary>
-        public static void Subscribe(this bool condition, ref Action del, Action action)
-        {
-            if (condition) del += action;
-            else del -= action;
-        }
-
-
+        if (condition) del += action;
+        else del -= action;
     }
+
+    public static void Subscribe<T>(this bool condition, ref Action<T> del, Action<T> action)
+    {
+        if (condition) del += action;
+        else del -= action;
+    }
+
+    /// <summary>
+    /// Apply a subscription to a specified Action, that can be a method group
+    /// </summary>
+    public static void Subscribe(this bool condition, ref Action del, Action action)
+    {
+        if (condition) del += action;
+        else del -= action;
+    }
+
+
 }

46b7380ed5cfbf94fa7c3ad495339c51ec1e88f1
Xavier Arpa <arpaxavier@gmail.com>
2022-06-14 00:49:52 +0200

    Se hace las validaciones

--- Runtime/Middleware_Generic_Parameter_Return_Publish.cs
+++ Runtime/Middleware_Generic_Parameter_Return_Publish.cs
@@ -4,7 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 #endregion
-
+ 
 public static partial class Middleware<TParameter, TReturn>
 {
     #region Variables

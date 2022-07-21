f1bef396c219d485e796c108f00d2407e0b1602a
Xavier Arpa <arpaxavier@gmail.com>
2022-07-21 02:10:47 +0200

    - Avances.

--- Runtime/ReactiveExtensorUtils.cs
+++ Runtime/ReactiveExtensorUtils.cs
@@ -21,10 +21,17 @@ public static partial class ReactiveExtensorUtils
     /// <summary>
     /// Apply a subscription to a specified Action, that can be a method group
     /// </summary>
-    public static void Subscribe(this bool condition, ref Action del, Action action)
+    public static void Subscribe(this bool condition, ref Action del, ref Action action)
     {
         if (condition) del += action;
         else del -= action;
+    }
+
+
+    public static void SubscribeOnce(this bool condition, ref Action del, Action action)
+    {
+        if (condition) del = action;
+        else del = null;
     }
 
     /// <summary>
